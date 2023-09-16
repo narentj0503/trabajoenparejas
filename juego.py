9745913d619a0c8199ea6443acb5f64c930e8b5e
narentj0503 <gnarent@gmail.com>
2023-09-16 15:30:44 -0500

    primer parte del trabajo
    se importa el random y se da las instrucciones

---
+++ juego.py
@@ -0,0 +1,14 @@
+import random
+
+juegoo=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+
+print("Bienvenido")
+print("la indicacion de este juego es la siguiente:")
+print("Tienes que adivinar el numero que hemos pensado, esta en un rango de 1 a 30")
+print("Para ello tienes 5 vidas o 5 intentos")
+print("¡mucha Suerte! :D ")
+print("¡mucha Suerte! :D :D ")
+
+vidas=5
+
+juego = random.choice(juegoo)
