fc2124787ca603913af9d14f6b9aa47ff14e54bb
estebanrave20 <128821356+estebanrave20@users.noreply.github.com>
2023-09-16 16:03:05 -0500

    parte2esteban
    partedos

--- juego.py
+++ juego.py
@@ -1,14 +1,24 @@
 import random
 
-juegoo=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+juego = random.randint(1, 30)  # Genera un número aleatorio entre 1 y 30
+vidas = 5
 
 print("Bienvenido")
-print("la indicacion de este juego es la siguiente:")
-print("Tienes que adivinar el numero que hemos pensado, esta en un rango de 1 a 30")
+print("La indicación de este juego es la siguiente:")
+print("Tienes que adivinar el número que hemos pensado, está en un rango de 1 a 30")
 print("Para ello tienes 5 vidas o 5 intentos")
-print("¡mucha Suerte! :D ")
-print("¡mucha Suerte! :D :D ")
+print("¡Mucha suerte! :D\n")
 
-vidas=5
-
-juego = random.choice(juegoo)
+while vidas > 0:
+    try:
+        intento = int(input("Ingresa tu adivinanza (1-30): "))
+        if 1 <= intento <= 30:
+            if intento == juego:
+                print(f"¡Felicidades! Has adivinado el número {juego}. ¡Ganaste!")
+                break
+            else:
+                vidas -= 1
+                if vidas > 0:
+                    if intento < juego:
+                        print(f"El número es mayor. Te quedan {vidas} vidas.")
+                  
